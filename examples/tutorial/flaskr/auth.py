68eb73d1c1b0ccf6f581474d451320d8625ed1bb
“thomasuriel1” <tlauravargas@gmail.com>
2024-08-14 11:59:10 -0300

    añadi el cambio de email en la pagina

--- examples/tutorial/flaskr/auth.py
+++ examples/tutorial/flaskr/auth.py
@@ -119,3 +119,25 @@ def logout():
     """Clear the current session, including the stored user id."""
     session.clear()
     return redirect(url_for("index"))
+
+
+@bp.route('/mod', methods=('GET', 'POST'))
+def updateEmail():
+    if request.method == 'POST':
+        email = request.form['new_email']
+        error = None
+        db = get_db()
+
+        if not email:
+            error = "Email is Required"
+
+        if error is None:
+            db.execute(
+                'UPDATE user SET email = ? WHERE id = ?',
+                (email, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('index'))
+        else:
+            flash(error)
+    return render_template('auth/update-mail.html')
